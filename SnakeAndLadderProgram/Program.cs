38c570b27abb81c837ed02700f27843cc8e6afaf
Shreya0207 <shanu.shreya270@gmail.com>
2020-10-02 14:27:34 +0530

    [Shreya]Add: Playing with the two player

--- SnakeAndLadderProgram/Program.cs
+++ SnakeAndLadderProgram/Program.cs
@@ -4,56 +4,26 @@ namespace SnakeAndLadderProgram
 {
     class Program
     {
-        public const int STARTING_POSITION = 0;
         public const int ENDING_POSITION = 100;
-        public const int NO_PLAY = 1;
-        public const int LADDER = 2;
-        public const int SNAKE = 3;
-        public static int DiceRoll()
-        {
-            Random random = new Random();
-            int diceNumber = random.Next(1, 7);
-            return diceNumber;
-        }
-        public static int newPosition(int diceNumber)
-        {
-            int toAdd = 0;
-            Random random = new Random();
-            int toDo = random.Next(1, 4);
-            switch (toDo)
-            {
-                case NO_PLAY:
-                    break;
-                case LADDER:
-                    toAdd = diceNumber;
-                    break;
-                case SNAKE:
-                    toAdd = -diceNumber;
-                    break;
-            }
-            return toAdd;
-        }
         static void Main(string[] args)
         {
-            int currentPosition = STARTING_POSITION, nextPosition;
-            int throws = 0;
-            while (currentPosition < ENDING_POSITION)
+            TwoPlayer player1 = new TwoPlayer();
+            TwoPlayer player2 = new TwoPlayer();
+            while (player1.currentPosition < ENDING_POSITION && player2.currentPosition < ENDING_POSITION)
             {
-                int diceNumber = DiceRoll();
-                throws++;
-                int toAdd = newPosition(diceNumber);
-                if (currentPosition + toAdd > ENDING_POSITION)
-                    nextPosition = currentPosition;
-                else
-                    nextPosition = currentPosition + toAdd;
-                if (nextPosition < STARTING_POSITION)
-                    currentPosition = STARTING_POSITION;
-                else
-                    currentPosition = nextPosition;
-                Console.WriteLine("Position After Die Roll No. " + throws + " is " + currentPosition);
+                player1.playATurn();
+                player2.playATurn();
             }
-            Console.WriteLine("No. Of Total Die Rolls : " + throws);
-            Console.WriteLine("Final Position of Player : " + currentPosition);
+            if (player1.currentPosition == ENDING_POSITION)
+            {
+                Console.WriteLine("Player 1 Won");
+            }
+            else
+            {
+                Console.WriteLine("Player 2 Won");
+            }
+            Console.WriteLine("Final Position of Player 1 :" + player1.currentPosition);
+            Console.WriteLine("Final Position of Player 2 :" + player2.currentPosition);
         }
     }
 }
---
+++ SnakeAndLadderProgram/TwoPlayer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeAndLadderProgram
+{
+    class TwoPlayer
+    {
+        public const int STARTING_POSITION = 0;
+        public const int ENDING_POSITION = 100;
+        public const int NO_PLAY = 1;
+        public const int LADDER = 2;
+        public const int SNAKE = 3;
+        public int currentPosition = STARTING_POSITION, nextPosition, throws = 0;
+        public int DiceRoll()
+        {
+            Random random = new Random();
+            int diceNumber = random.Next(1, 7);
+            return diceNumber;
+        }
+        public int newPosition(int diceNumber)
+        {
+            int toAdd = 0;
+            Random random = new Random();
+            int toDo = random.Next(1, 4);
+            switch (toDo)
+            {
+                case NO_PLAY:
+                    break;
+                case LADDER:
+                    toAdd = diceNumber;
+                    break;
+                case SNAKE:
+                    toAdd = -diceNumber;
+                    break;
+            }
+            return toAdd;
+        }
+        public void playATurn()
+        {
+            int diceNumber = this.DiceRoll();
+            this.throws++;
+            int toAdd = this.newPosition(diceNumber);
+            if (toAdd > 0)
+            {
+                this.playATurn();
+            }
+            if (this.currentPosition + toAdd > ENDING_POSITION)
+            {
+                this.nextPosition = this.currentPosition;
+            }
+            else
+            {
+                this.nextPosition = this.currentPosition + toAdd;
+            }
+            if (this.nextPosition < STARTING_POSITION)
+            {
+                this.currentPosition = STARTING_POSITION;
+            }
+            else
+            {
+                this.currentPosition = this.nextPosition;
+            }
+        }
+    }
+}
+            
