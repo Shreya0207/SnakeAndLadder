81ec49320fc708804035bf53635c8b47c55170c9
Shreya0207 <shanu.shreya270@gmail.com>
2020-10-02 13:07:46 +0530

    [Shreya]Add: Roll the die

--- SnakeAndLadderProgram/Program.cs
+++ SnakeAndLadderProgram/Program.cs
@@ -6,11 +6,15 @@ namespace SnakeAndLadderProgram
     {
         public const int STARTING_POSITION = 0;
         public const int ENDING_POSITION = 100;
+        public static int DiceRoll()
+        {
+            Random random = new Random();
+            int diceNumber = random.Next(1, 7);
+            return diceNumber;
+        }
         static void Main(string[] args)
         {
-            int currentPosition = STARTING_POSITION; 
-            Console.WriteLine("Welcome to the Snake and Ladder game");
-            Console.WriteLine("Player Position: " + currentPosition);
+            int diceNumber = DiceRoll();
         }
     }
 }
