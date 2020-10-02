4b88f44ed14e277ce68d15a6985fe271d691e7ba
Shreya0207 <shanu.shreya270@gmail.com>
2020-10-02 12:54:28 +0530

    [Shreya] Add: Initial Position

--- SnakeAndLadderProgram/Program.cs
+++ SnakeAndLadderProgram/Program.cs
@@ -4,9 +4,13 @@ namespace SnakeAndLadderProgram
 {
     class Program
     {
+        public const int STARTING_POSITION = 0;
+        public const int ENDING_POSITION = 100;
         static void Main(string[] args)
         {
+            int currentPosition = STARTING_POSITION; 
             Console.WriteLine("Welcome to the Snake and Ladder game");
+            Console.WriteLine("Player Position: " + currentPosition);
         }
     }
 }
